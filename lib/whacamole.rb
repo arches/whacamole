4dd3460fe416845ebedb1dcea41b3de5ff4a27cd
Chris Doyle <archslide@gmail.com>
2013-08-29 09:42:51 -0500

    top level event loop

--- lib/whacamole.rb
+++ lib/whacamole.rb
@@ -11,4 +11,20 @@ module Whacamole
     @@config[app_name.to_s] ||= Config.new(app_name)
     yield @@config[app_name.to_s]
   end
+
+  def self.monitor
+    threads = []
+    @@config.each do |app_name, config|
+      threads << Thread.new do
+        heroku = HerokuWrapper.new(app_name, config.api_token)
+
+        while true
+          stream_url = heroku.create_log_session
+          Stream.new(stream_url, heroku, &config.event_handler).watch
+        end
+      end
+    end
+    threads.collect(&:join)
+  end
 end
+
